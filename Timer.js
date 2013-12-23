276d49927f4aacea569a278872f81d16798bd73e
Pierre Aboucaya <p@asper.fr>
2013-12-23 11:37:28 +0100

    Create Timer.js

---
+++ Timer.js
@@ -0,0 +1,57 @@
+
+var Timer = new Class({
+	Implements: [Events, Options],
+	options: {
+		start: 0,
+		step: 1,
+		end: 30,
+		onBeforeSetOptions: function(){},
+		onAfterSetOptions: function(){},
+		onBeforeStart: function(){},
+		onAfterStart: function(){},
+		onBeforeUpdate: function(){},
+		onAfterUpdate: function(){},
+		onBeforeStop: function(){},
+		onAfterStop: function(){}
+	},
+	iterator: null,
+	time: 0,
+	initialize: function(options){
+		var t = this;
+		t.fireEvent('beforeSetOptions');
+		t.setOptions(options);
+		t.time = t.options.start.toInt();
+		t.fireEvent('afterSetOptions');
+		return t;
+	},
+	start: function(){
+		var t = this;
+		t.fireEvent('beforeStart');
+		t.iterator = t.iterate.periodical(1000, t);
+		t.fireEvent('afterStart');
+		return t;
+	},
+	iterate: function(){
+		var t = this,
+			o = t.options,
+			step = o.step.toInt(),
+			end = o.end.toInt();
+		t.fireEvent('beforeUpdate');
+		t.time += step;
+		if(
+			(step > 0 && t.time >= end)
+			|| (step < 0 && t.time <= end)
+		){
+			t.stop();
+		}
+		t.fireEvent('afterUpdate');
+		return t;
+	},
+	stop: function(){
+		var t = this;
+		t.fireEvent('beforeStop');
+		clearInterval(t.iterator);
+		t.fireEvent('afterStop');
+		return t;
+	}
+});
